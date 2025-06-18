73cf653fb5d1c8a4f4e6e9f1ab9a4978a7f4d7bf
juandhm18 <juandhm20@gmail.com>
2025-06-18 13:35:29 -0500

    work updates

--- JavaScript/EntrenamientoM3S1/sistema_interactivo.js
+++ JavaScript/EntrenamientoM3S1/sistema_interactivo.js
@@ -8,7 +8,7 @@ edad = parseInt(edad);
 if(isNaN(edad)){
     console.error("Error: Por favor, Digita una edad valida en numeros.");
 } else if (edad<18){
-    alert('Hola '+nombre+' eres menor de edad. !Sigue aprendiendo y disfrutando del codigo!');
+    alert(`Hola ${nombre} eres menor de edad. !Sigue aprendiendo y disfrutando del codigo!`);
 } else{
-    alert("Hola "+nombre+" eres mayor de edad. !Preparate para grandes oportunidades en el mundo de la programación!");
+    alert(`Hola ${nombre} eres mayor de edad. !Preparate para grandes oportunidades en el mundo de la programación!`);
 }
